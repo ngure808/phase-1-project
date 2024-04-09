8032d91a3c3d10ac73e16c0ae7f0dc1c22961a22
ngure808 <mwangingure808@gmail.com>
2024-04-09 09:57:56 +0300

    displays names and images

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+        // let div = document.getElementById("card")
+        let ul = document.getElementById("list")
+        let localURL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+        
+    
+        fetch(localURL)
+        .then(res => res.json())
+        .then(values => {
+            console.log(values[0].product_type)
+            values.map(value => preUI(value))
+        })
+        .catch(e => console.log(e.message))
+    
+        function preUI(value){
+            let title = document.createElement("h4")
+            title.setAttribute("id", "card")
+            title.innerText = value.name
+            ul.appendChild(title)
+            console.log(value.product_type)
+    
+            let image = document.createElement("img")
+            image.src = value.image_link
+            ul.appendChild(image)
+        }
 })
