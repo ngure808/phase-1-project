518cd2100f4eb3fb8f8a8516bee37a55a48e30c4
ngure808 <mwangingure808@gmail.com>
2024-04-10 18:17:07 +0300

    each buuton displays its content without reload

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let nailpolish = document.getElementById("nailpolish")
     let lipstick = document.getElementById("lipstick")
     let mascara = document.getElementById("mascara")
-    let header = document.getElementById("card")
     let div = document.getElementById("section")
     let localURL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-    let button = document.getElementsByTagName("button")
-
+    // let button = document.getElementsByTagName("button")
 
     function mainSpace(){
         fetch(localURL)
@@ -21,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
             for(item of values){
             preUI(item)
             }
+            console.log(values)
         })
         .catch(e => console.log(e.message))
     }
@@ -28,11 +27,35 @@ document.addEventListener("DOMContentLoaded", () => {
     
     function preUI(value){
        bronzer.addEventListener("click" , () => {
-        buttons(value)
+        bronzers(value)
+       })
+       blush.addEventListener("click", () => {
+        blushes(value)
+       })
+       lipliner.addEventListener("click", () => {
+        lipline(value)
+       })
+       foundation.addEventListener("click", () => {
+        found(value)
+       })
+       eyeshadow.addEventListener("click", () => {
+        shadow(value)
+       })
+       eyeliner.addEventListener("click", () => {
+        liner(value)
+       })
+       nailpolish.addEventListener("click", () => {
+        cutex(value)
+       })
+       lipstick.addEventListener("click", () => {
+        lips(value)
+       })
+       mascara.addEventListener("click", () => {
+        masc(value)
        })
     }
-    function buttons(value){
-        let title = document.createElement("h4")
+    function bronzers(value){
+        let title = document.createElement("h3")
         title.setAttribute("id", "card")
         let image = document.createElement("img")
         let para = document.createElement("p")
@@ -56,6 +79,222 @@ document.addEventListener("DOMContentLoaded", () => {
             para.appendChild(btn)   
         }
     }
+
+    function blushes(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "blush"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+    function lipline(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "lip_liner"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function found(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "foundation"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function shadow(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "eyeshadow"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function liner(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "eyeliner"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function cutex(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "nail_polish"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function lips(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "lipstick"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
+    function masc(value){
+        let title = document.createElement("h3")
+        title.setAttribute("id", "card")
+        let image = document.createElement("img")
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
+
+        if(value.product_type === "mascara"){
+            title.innerText = value.name
+            div.appendChild(title)
+
+            image.src = value.image_link
+            div.appendChild(image)
+
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
+
+    }
+
 
 
 })
