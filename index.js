dde3829637a5ef698cfdf5b7f47039beeabdfdfa
ngure808 <mwangingure808@gmail.com>
2024-04-10 17:06:30 +0300

    upon clicking button bronzer content is loaded

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    // let div = document.getElementById("card")
-    let ul = document.getElementById("content")
+    let bronzer = document.getElementById("bronzer")
+    let  blush = document.getElementById("blush")
+    let lipliner = document.getElementById("lipliner")
+    let foundation = document.getElementById("foundation")
+    let eyeshadow= document.getElementById("eyeshadow")
+    let eyeliner = document.getElementById("eyeliner")
+    let nailpolish = document.getElementById("nailpolish")
+    let lipstick = document.getElementById("lipstick")
+    let mascara = document.getElementById("mascara")
+    let header = document.getElementById("card")
+    let div = document.getElementById("section")
     let localURL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+    let button = document.getElementsByTagName("button")
+
 
     function mainSpace(){
         fetch(localURL)
         .then(res => res.json())
         .then(values => {
-            console.log(values)
-            values.map(value => preUI(value))
+            for(item of values){
+            preUI(item)
+            }
         })
         .catch(e => console.log(e.message))
     }
     mainSpace()
     
     function preUI(value){
+       bronzer.addEventListener("click" , () => {
+        buttons(value)
+       })
+    }
+    function buttons(value){
         let title = document.createElement("h4")
         title.setAttribute("id", "card")
-        title.innerText = value.name
-        ul.appendChild(title)
-
         let image = document.createElement("img")
-        image.src = value.image_link
-        ul.appendChild(image)
+        let para = document.createElement("p")
+        para.setAttribute('id', "summary")
+        let price = document.createElement("h5")
+        let btn = document.createElement("button")
 
-       let para = document.createElement("p")
-       para.innerText = value.description
-       ul.appendChild(para)
+        if(value.product_type === "bronzer"){
+            title.innerText = value.name
+            div.appendChild(title)
 
-       let price = document.createElement("h5")
-       price.innerText = `${value.price} $`
-       para.appendChild(price)
+            image.src = value.image_link
+            div.appendChild(image)
 
-       
+            para.innerText = value.description
+            div.appendChild(para)
+
+            price.innerText = `Price = ${value.price} $`
+            para.appendChild(price)
+            btn.innerText = "Order"
+            para.appendChild(btn)   
+        }
     }
 
 
